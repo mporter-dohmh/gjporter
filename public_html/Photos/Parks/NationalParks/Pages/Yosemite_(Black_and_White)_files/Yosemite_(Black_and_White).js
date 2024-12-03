1d703a52e490d2998dd4b87b36c48f762c2027d3
mporter1@health.nyc.gov <mporter1@health.nyc.gov>
2024-12-02 21:02:34 -0500

    adding files 13810 to 13910

---
+++ public_html/Photos/Parks/NationalParks/Pages/Yosemite_(Black_and_White)_files/Yosemite_(Black_and_White).js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.4 local-build-20130411
+
+function createMediaStream_id2()
+{return IWCreatePhotocast("http://home.comcast.net/%7Egjporter/Parks/NationalParks/Pages/Yosemite_%28Black_and_White%29_files/rss.xml",true);}
+function initializeMediaStream_id2()
+{createMediaStream_id2().load('http://home.comcast.net/%7Egjporter/Parks/NationalParks/Pages',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget0'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id2',{pageIndex:0}));});}
+function layoutMediaGrid_id2(range)
+{createMediaStream_id2().load('http://home.comcast.net/%7Egjporter/Parks/NationalParks/Pages',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(4,new IWSize(124,124),new IWSize(124,0),new IWSize(153,139),27,27,0,new IWSize(0,0)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:0,reflectionOffset:2,captionHeight:0,fullScreen:0,transitionIndex:2},'../../Media/slideshow.html','widget0','widget1','widget2')});}
+function relayoutMediaGrid_id2(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id2(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id2(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','../../Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','../../Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Yosemite_(Black_and_White)_files/Yosemite_(Black_and_White)Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');NotificationCenter.addObserver(null,relayoutMediaGrid_id2,'RangeChanged','id2')
+Widget.onload();fixAllIEPNGs('../../Media/transparent.gif');fixupIECSS3Opacity('id3');initializeMediaStream_id2()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
