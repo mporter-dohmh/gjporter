3a31d4ec97564e41fffb073794f92323b259f47a
mporter1@health.nyc.gov <mporter1@health.nyc.gov>
2024-12-02 20:11:00 -0500

    adding files 2610 to 2710

---
+++ public_html/NOLA2/Photos/Gambia/Gambia_files/Gambia.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.4 local-build-20120425
+
+function createMediaStream_id2()
+{return IWCreatePhotocast("http://home.comcast.net/%7Egjporter/Gambia/Gambia_files/rss.xml",false);}
+function initializeMediaStream_id2()
+{createMediaStream_id2().load('http://home.comcast.net/%7Egjporter/Gambia',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget0'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id2',{pageIndex:0}));});}
+function layoutMediaGrid_id2(range)
+{createMediaStream_id2().load('http://home.comcast.net/%7Egjporter/Gambia',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(4,new IWSize(124,124),new IWSize(124,0),new IWSize(153,139),27,27,0,new IWSize(2,2)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'Media/slideshow.html','widget0','widget1','widget2')});}
+function relayoutMediaGrid_id2(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id2(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id2(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Gambia_files/GambiaMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');NotificationCenter.addObserver(null,relayoutMediaGrid_id2,'RangeChanged','id2')
+fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id3');initializeMediaStream_id2()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
